a18290f4863af5f8c0611459e5fc6d5a44d2edca
Julien Lengrand-Lambert <Julien@lengrand.fr>
2022-01-13 11:21:25 +0100

    Removes unused method

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -1,12 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Net;
 using Adyen;
 using Adyen.Model.Checkout;
 using Adyen.Service;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using static Adyen.Model.Checkout.PaymentDetailsResponse;
 
 namespace adyen_dotnet_online_payments.Controllers
 {
@@ -22,25 +19,6 @@ namespace adyen_dotnet_online_payments.Controllers
             var client = new Client(Environment.GetEnvironmentVariable("ADYEN_API_KEY"), Adyen.Model.Enum.Environment.Test); // Test Environment;
             _checkout = new Checkout(client);
             _merchant_account = Environment.GetEnvironmentVariable("ADYEN_MERCHANT");
-        }
-
-        [HttpPost("api/getPaymentMethods")]
-        public ActionResult<string> GetPaymentMethods([FromBody] PaymentMethodsRequest req)
-        {
-            _logger.LogInformation($"Request for PaymentMethods API::\n{req}\n");
-
-            req.MerchantAccount = _merchant_account;
-            req.Channel = PaymentMethodsRequest.ChannelEnum.Web;
-
-            try
-            {
-                return _checkout.PaymentMethods(req).ToJson();
-            }
-            catch (Adyen.HttpClient.HttpClientException e)
-            {
-                _logger.LogError($"Request for PaymentMethods failed::\n{e.ResponseBody}\n");
-                throw e;
-            }
         }
 
         [HttpPost("api/sessions")]
