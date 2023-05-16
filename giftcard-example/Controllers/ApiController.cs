8d9e4fb8bfe9b1869f0a43f4cb388bb0e459526d
Kwok He Chu <105217051+Kwok-he-Chu@users.noreply.github.com>
2023-05-16 14:32:40 +0200

    Update giftcard request

--- giftcard-example/Controllers/ApiController.cs
+++ giftcard-example/Controllers/ApiController.cs
@@ -31,24 +31,26 @@ namespace adyen_dotnet_giftcard_example.Controllers
         [HttpPost("api/sessions/dropin")]
         public async Task<ActionResult<string>> SessionsDropin(CancellationToken cancellationToken = default)
         {
-            var sessionsRequest = new CreateCheckoutSessionRequest();
-            sessionsRequest.MerchantAccount = _merchantAccount; // Required.
-            sessionsRequest.Channel = CreateCheckoutSessionRequest.ChannelEnum.Web;
-            sessionsRequest.Amount = new Amount("EUR", 11000); // Value is 110€ in minor units
-
             var orderRef = Guid.NewGuid();
-            sessionsRequest.Reference = orderRef.ToString(); // Required.
-
-            // Required for 3DS2 redirect flow.
-            sessionsRequest.ReturnUrl = $"{_urlService.GetHostUrl()}/dropin/redirect?orderRef={orderRef}"; // Redirect from dropin.
-
-            // Used for klarna, klarna is not supported everywhere, hence why we've defaulted to countryCode "NL" as it supports the following payment methods below:
-            // "Pay now", "Pay later" and "Pay over time", see docs for more info: https://docs.adyen.com/payment-methods/klarna#supported-countries
-            sessionsRequest.CountryCode = "NL";
-            sessionsRequest.LineItems = new List<LineItem>()
+            var sessionsRequest = new CreateCheckoutSessionRequest()
             {
-                new LineItem(quantity: 1, amountIncludingTax: 5500 , description: "Sunglasses"),
-                new LineItem(quantity: 1, amountIncludingTax: 5500, description: "Headphones")
+                MerchantAccount = _merchantAccount, // Required.
+                Reference = orderRef.ToString(), // Required.
+
+                Channel = CreateCheckoutSessionRequest.ChannelEnum.Web,
+                Amount = new Amount("EUR", 11000), // Value is 110€ in minor units
+
+                // Required for 3DS2 redirect flow.
+                ReturnUrl = $"{_urlService.GetHostUrl()}/dropin/redirect?orderRef={orderRef}", // Redirect from dropin.
+
+                // Used for klarna, klarna is not supported everywhere, hence why we've defaulted to countryCode "NL" as it supports the following payment methods below:
+                // "Pay now", "Pay later" and "Pay over time", see docs for more info: https://docs.adyen.com/payment-methods/klarna#supported-countries
+                CountryCode = "NL",
+                LineItems = new List<LineItem>()
+                {
+                    new LineItem(quantity: 1, amountIncludingTax: 5500, description: "Sunglasses"),
+                    new LineItem(quantity: 1, amountIncludingTax: 5500, description: "Headphones")
+                }
             };
 
             try
@@ -67,24 +69,26 @@ namespace adyen_dotnet_giftcard_example.Controllers
         [HttpPost("api/sessions/giftcardcomponent")]
         public async Task<ActionResult<string>> SessionsGiftcardComponent(CancellationToken cancellationToken = default)
         {
-            var sessionsRequest = new CreateCheckoutSessionRequest();
-            sessionsRequest.MerchantAccount = _merchantAccount; // Required.
-            sessionsRequest.Channel = CreateCheckoutSessionRequest.ChannelEnum.Web;
-            sessionsRequest.Amount = new Amount("EUR", 11000); // Value is 110€ in minor units.
-
             var orderRef = Guid.NewGuid();
-            sessionsRequest.Reference = orderRef.ToString(); // Required.
-
-            // Required for 3DS2 redirect flow.
-            sessionsRequest.ReturnUrl = $"{_urlService.GetHostUrl()}/giftcardcomponent/redirect?orderRef={orderRef}"; // Redirect from GiftcardComponent.
-
-            // Used for klarna, klarna is not supported everywhere, hence why we've defaulted to countryCode "NL" as it supports the following payment methods below:
-            // "Pay now", "Pay later" and "Pay over time", see docs for more info: https://docs.adyen.com/payment-methods/klarna#supported-countries
-            sessionsRequest.CountryCode = "NL";
-            sessionsRequest.LineItems = new List<LineItem>()
+            var sessionsRequest = new CreateCheckoutSessionRequest()
             {
-                new LineItem(quantity: 1, amountIncludingTax: 5500 , description: "Sunglasses"),
-                new LineItem(quantity: 1, amountIncludingTax: 5500, description: "Headphones")
+                MerchantAccount = _merchantAccount, // Required.
+                Reference = orderRef.ToString(), // Required.
+                
+                Channel = CreateCheckoutSessionRequest.ChannelEnum.Web,
+                Amount = new Amount("EUR", 11000), // Value is 110€ in minor units.
+
+                // Required for 3DS2 redirect flow.
+                ReturnUrl = $"{_urlService.GetHostUrl()}/giftcardcomponent/redirect?orderRef={orderRef}", // Redirect from GiftcardComponent.
+
+                // Used for klarna, klarna is not supported everywhere, hence why we've defaulted to countryCode "NL" as it supports the following payment methods below:
+                // "Pay now", "Pay later" and "Pay over time", see docs for more info: https://docs.adyen.com/payment-methods/klarna#supported-countries
+                CountryCode = "NL",
+                LineItems = new List<LineItem>()
+                {
+                    new LineItem(quantity: 1, amountIncludingTax: 5500, description: "Sunglasses"),
+                    new LineItem(quantity: 1, amountIncludingTax: 5500, description: "Headphones")
+                }
             };
 
             try
