c1fd4ac0a562bf1cf33e1e388ff0ef41157cd7fb
Kwok He Chu <105217051+Kwok-he-Chu@users.noreply.github.com>
2023-06-06 09:57:54 +0200

    Initial commit
    Added structure and folders for 3-step-checkout
    Added correct namespace names
    Removed .vscode specific user settings

---
+++ checkout-example-advanced/Controllers/WebhookController.cs
@@ -0,0 +1,81 @@
+using Adyen.Model.Notification;
+using Adyen.Util;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using adyen_dotnet_checkout_example_advanced.Options;
+
+namespace adyen_dotnet_checkout_example_advanced.Controllers
+{
+    [ApiController]
+    public class WebhookController : ControllerBase
+    {
+        private readonly ILogger<WebhookController> _logger;
+        private readonly HmacValidator _hmacValidator;
+        private readonly string _hmacKey;
+        
+        public WebhookController(ILogger<WebhookController> logger, IOptions<AdyenOptions> options, HmacValidator hmacValidator)
+        {
+            _logger = logger;
+            _hmacKey = options.Value.ADYEN_HMAC_KEY;
+            _hmacValidator = hmacValidator;
+        }
+
+        [HttpPost("api/webhooks/notifications")]
+        public async Task<ActionResult<string>> Webhooks(NotificationRequest notificationRequest)
+        {
+            _logger.LogInformation($"Webhook received::\n{notificationRequest}\n");
+
+            try
+            {
+                // JSON and HTTP POST notifications always contain a single `NotificationRequestItem` object.
+                // Read more: https://docs.adyen.com/development-resources/webhooks/understand-notifications#notification-structure.
+                NotificationRequestItemContainer container = notificationRequest.NotificationItemContainers?.FirstOrDefault();
+
+                if (container == null)
+                {
+                    return BadRequest("Container has no notification items.");
+                }
+
+                // We always recommend to activate HMAC validation in the webhooks for security reasons.
+                // Read more here: https://docs.adyen.com/development-resources/webhooks/verify-hmac-signatures & https://docs.adyen.com/development-resources/webhooks#accept-notifications.
+                if (!_hmacValidator.IsValidHmac(container.NotificationItem, _hmacKey))
+                {
+                    _logger.LogError($"Error while validating HMAC Key");
+                    return BadRequest("[not accepted invalid hmac key]");
+                }
+
+                // Return if webhook is not successful.
+                if (!container.NotificationItem.Success)
+                {
+                    _logger.LogError($"Webhook unsuccessful: {container.NotificationItem.Reason} \n" +
+                        $"EventCode: {container.NotificationItem.EventCode}");
+                    return Ok("[accepted]"); // The webhook was delivered (but was unsuccessful), hence why we'll return a [accepted] response to confirm that we've received it.
+                }
+
+                // Process notification asynchronously.
+                await ProcessNotificationAsync(container.NotificationItem);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("Exception thrown: " + e.ToString());
+                throw;
+            }
+
+            return Ok("[accepted]");
+        }
+
+        private Task ProcessNotificationAsync(NotificationRequestItem notification)
+        {        
+            // Perform your business logic or asynchronous operations (awaits) here.
+            // In this case, we just log it.
+            _logger.LogInformation($"Received webhook with event::\n" +
+                                   $"Merchant Reference ::{notification.MerchantReference} \n" +
+                                   $"PSP Reference ::{notification.PspReference} \n");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ checkout-example-advanced/Services/UrlService.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Linq;
+using adyen_dotnet_checkout_example_advanced.Controllers;
+
+namespace adyen_dotnet_checkout_example_advanced.Services
+{
+    public interface IUrlService
+    {
+        /// <summary>
+        /// Retrieves the host url dynamically using the origin in the headers of the request.
+        /// This is used to generate the correct host url when running on either http(s)://localhost:*, http(s)://*.gitpod or http(s)://*.devtunnels.ms.
+        /// For usage see <see cref="ApiController.Sessions()"/>.
+        /// [!] On production (or a dedicated dev environment), use a fixed URL.
+        /// </summary>
+        /// <returns>Returns the host url.</returns>
+        string GetHostUrl();
+    }
+
+    public class UrlService : IUrlService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UrlService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        /// <inheritdoc/>
+        public string GetHostUrl()
+        {
+            if (_httpContextAccessor.HttpContext?.Request?.Headers == null)
+                return null;
+
+            return GetForwardedHostUrl() ?? GetOriginUrl();
+        }
+
+        /// <summary>
+        /// Gets the origin url from the header request.
+        /// </summary>
+        /// <returns>The origin host url.</returns>
+        private string GetOriginUrl()
+        {
+            if (!_httpContextAccessor.HttpContext.Request.Headers.TryGetValue("Origin", out var result))
+                return null;
+
+            return result.FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Gets the url from a forwarded host.
+        /// If the request was forwarded (due to port tunneling f.e.), we build the url and return the forwarded host url instead.
+        /// </summary>
+        /// <returns>The forwarded host url.</returns>
+        private string GetForwardedHostUrl()
+        {
+            if (!_httpContextAccessor.HttpContext.Request.Headers.TryGetValue("x-forwarded-host", out var forwardedHosts))
+                return null;
+
+            string forwardedHost = forwardedHosts.FirstOrDefault();
+
+            if (forwardedHost == null)
+                return null;
+
+            if (!_httpContextAccessor.HttpContext.Request.Headers.TryGetValue("x-forwarded-scheme", out var forwardedSchemes))
+                return null;
+
+            string forwardedScheme = forwardedSchemes.FirstOrDefault();
+            return $"{forwardedScheme.Replace('{', ' ').Replace('}', ' ')}://{forwardedHost}";
+        }
+    }
+}
