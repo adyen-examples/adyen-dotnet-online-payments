7fdaf85219c55d7767abda9d50af3a2c1da53fa0
Kwok He Chu <105217051+Kwok-he-Chu@users.noreply.github.com>
2023-08-10 15:04:59 +0200

    Initial commit

---
+++ in-person-payments-example/Controllers/ApiController.cs
@@ -0,0 +1,68 @@
+using Adyen.Model.Checkout;
+using Adyen.Service.Checkout;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using adyen_dotnet_in_person_payments_example.Options;
+using adyen_dotnet_in_person_payments_example.Services;
+
+namespace adyen_dotnet_in_person_payments_example.Controllers
+{
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        private readonly ILogger<ApiController> _logger;
+        private readonly IUrlService _urlService;
+        private readonly IPaymentsService _paymentsService;
+        private readonly string _merchantAccount;
+        
+        public ApiController(IPaymentsService paymentsService, ILogger<ApiController> logger, IUrlService urlService, IOptions<AdyenOptions> options)
+        {
+            _logger = logger;
+            _urlService = urlService;
+            _paymentsService = paymentsService;
+            _merchantAccount = options.Value.ADYEN_MERCHANT_ACCOUNT;
+        }
+
+        [HttpPost("api/sessions")]
+        public async Task<ActionResult<CreateCheckoutSessionResponse>> Sessions(CancellationToken cancellationToken = default)
+        {
+            var orderRef = Guid.NewGuid();
+            var sessionsRequest = new CreateCheckoutSessionRequest()
+            {
+                MerchantAccount = _merchantAccount, // Required.
+                Reference = orderRef.ToString(), // Required.
+                Channel = CreateCheckoutSessionRequest.ChannelEnum.Web,
+                Amount = new Amount("EUR", 10000), // Value is 100€ in minor units.
+                
+                // Required for 3DS2 redirect flow.
+                ReturnUrl = $"{_urlService.GetHostUrl()}/redirect?orderRef={orderRef}",
+
+                // Used for klarna, klarna is not supported everywhere, hence why we've defaulted to countryCode "NL" as it supports the following payment methods below:
+                // "Pay now", "Pay later" and "Pay over time", see docs for more info: https://docs.adyen.com/payment-methods/klarna#supported-countries.
+                CountryCode = "NL",
+                LineItems = new List<LineItem>()
+                {
+                    new LineItem(quantity: 1, amountIncludingTax: 5000, description: "Sunglasses"),
+                    new LineItem(quantity: 1, amountIncludingTax: 5000, description: "Headphones")
+                }
+            };
+
+            try
+            {
+                var res = await _paymentsService.SessionsAsync(sessionsRequest, cancellationToken: cancellationToken);
+                _logger.LogInformation($"Response for Payments API:\n{res}\n");
+                return res;
+            }
+            catch (Adyen.HttpClient.HttpClientException e)
+            {
+                _logger.LogError($"Request for Payments failed:\n{e.ResponseBody}\n");
+                throw;
+            }
+        }
+    }
+}
---
+++ in-person-payments-example/Startup.cs
@@ -0,0 +1,99 @@
+using Adyen;
+using Adyen.Model;
+using Adyen.Service.Checkout;
+using Adyen.Util;
+using adyen_dotnet_in_person_payments_example.Options;
+using adyen_dotnet_in_person_payments_example.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+
+namespace adyen_dotnet_in_person_payments_example
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Configure your keys using the Options pattern.
+            // This will auto-retrieve/configure your keys from your environmental variables (ADYEN_CLIENT_KEY, ADYEN_API_KEY, ADYEN_MERCHANT_ACCOUNT, ADYEN_HMAC_KEY).
+            services.Configure<AdyenOptions>(
+                options =>
+                {
+                    // Public key used for client-side authentication: https://docs.adyen.com/development-resources/client-side-authentication.
+                    options.ADYEN_CLIENT_KEY = Configuration[nameof(AdyenOptions.ADYEN_CLIENT_KEY)];
+                    
+                    // Your secret API Key: https://docs.adyen.com/development-resources/api-credentials#generate-your-api-key.
+                    options.ADYEN_API_KEY = Configuration[nameof(AdyenOptions.ADYEN_API_KEY)];
+                    
+                    // Your Merchant Account name: https://docs.adyen.com/account/account-structure.
+                    options.ADYEN_MERCHANT_ACCOUNT = Configuration[nameof(AdyenOptions.ADYEN_MERCHANT_ACCOUNT)];
+
+                    // HMAC Key used to validate your webhook signatures: https://docs.adyen.com/development-resources/webhooks/verify-hmac-signatures.
+                    options.ADYEN_HMAC_KEY = Configuration[nameof(AdyenOptions.ADYEN_HMAC_KEY)]; 
+                }
+            );
+
+            // Register your controllers.
+            services.AddControllersWithViews();
+            services.AddControllers().AddNewtonsoftJson();
+
+            services.AddHttpContextAccessor()
+                .AddTransient<IUrlService, UrlService>();
+
+            // Register your dependencies.
+            services.AddSingleton(provider =>
+            {
+                var options = provider.GetRequiredService<IOptions<AdyenOptions>>();
+                return new Client(
+                    new Config()
+                    {
+                        // Get your `API Key`, `HMAC Key` and `MerchantAccount` from AdyenOptions using the Options pattern.
+                        XApiKey = options.Value.ADYEN_API_KEY,
+                        HmacKey = options.Value.ADYEN_HMAC_KEY,
+                        MerchantAccount = options.Value.ADYEN_MERCHANT_ACCOUNT,
+                        // Test environment.
+                        Environment = Environment.Test,
+                    });
+            }).AddHttpClient(); // Add HttpClient.
+
+            services.AddSingleton<IPaymentsService, PaymentsService>(); // Used to be called "Checkout.cs" in Adyen .NET 9.x.x and below, see https://github.com/Adyen/adyen-dotnet-api-library/blob/9.2.1/Adyen/Service/Checkout.cs.
+            services.AddSingleton<HmacValidator>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/error");
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
