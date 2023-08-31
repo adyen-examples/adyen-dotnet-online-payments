e39f910bfd204c74048956f89424b778872e8d68
Kwok He Chu <105217051+Kwok-he-Chu@users.noreply.github.com>
2023-08-31 11:03:52 +0200

    Do not throw nullargument exception when POS_POI_ID is empty

--- in-person-payments-example/Startup.cs
+++ in-person-payments-example/Startup.cs
@@ -34,7 +34,7 @@ namespace adyen_dotnet_in_person_payments_example
                     options.ADYEN_API_KEY = Configuration[nameof(AdyenOptions.ADYEN_API_KEY)];
                     options.ADYEN_MERCHANT_ACCOUNT = Configuration[nameof(AdyenOptions.ADYEN_MERCHANT_ACCOUNT)];
                     options.ADYEN_HMAC_KEY = Configuration[nameof(AdyenOptions.ADYEN_HMAC_KEY)];
-                    options.ADYEN_POS_POI_ID = Configuration[nameof(AdyenOptions.ADYEN_POS_POI_ID)] ?? throw new ArgumentNullException();
+                    options.ADYEN_POS_POI_ID = Configuration[nameof(AdyenOptions.ADYEN_POS_POI_ID)];
                     options.ADYEN_POS_SALE_ID = "SALE_ID_POS_42";
                 }
             );
