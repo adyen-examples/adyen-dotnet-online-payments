df77715c073fe39e3f30d97e6b5bc90732b465db
Ayodeji Ogundare <63319309+ayodejidev@users.noreply.github.com>
2025-12-23 16:12:21 +0100

    rebase files

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -11,5 +11,17 @@ dotnet dev-certs https || true
 echo "[setup] restoring solution"
 dotnet restore adyen-dotnet-online-payments.sln
 
-echo "[setup] done. You can start a project with, e.g.:"
-echo "  cd checkout-example && dotnet run"
+cat <<'EOF'
+[setup] next steps:
+
+Before starting the app, set these environment variables (export in terminal):
+  ADYEN_API_KEY          (required) (https://docs.adyen.com/user-management/how-to-get-the-api-key)
+  ADYEN_CLIENT_KEY       (required) (https://docs.adyen.com/user-management/client-side-authentication)
+  ADYEN_MERCHANT_ACCOUNT (required) (https://docs.adyen.com/account/account-structure)
+  ADYEN_HMAC_KEY         (optional, recommended) (https://docs.adyen.com/development-resources/webhooks/verify-hmac-signatures)
+
+Then navigate to your desired example directory and start the application:
+    cd <example-directory> (e.g. checkout-example)
+    dotnet run
+    visit http://localhost:8080 (or https://localhost:5001)
+EOF
