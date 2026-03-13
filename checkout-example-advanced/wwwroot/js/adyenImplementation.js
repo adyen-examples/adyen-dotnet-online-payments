90f687d98eaabbaf814ee6a7459691f4225118a6
Kwok He <105217051+Kwok-he-Chu@users.noreply.github.com>
2026-03-13 18:54:07 +0100

    Upgrade Frontend to V6-dropin

--- checkout-example-advanced/wwwroot/js/adyenImplementation.js
+++ checkout-example-advanced/wwwroot/js/adyenImplementation.js
@@ -1,107 +1,150 @@
 const clientKey = document.getElementById("clientKey").innerHTML;
-const type = document.getElementById("type").innerHTML;
+const { AdyenCheckout, Dropin } = window.AdyenWeb;
 
-async function initCheckout() {
+async function startCheckout() {
     try {
-        const paymentMethodsResponse = await sendPostRequest("/api/getPaymentMethods");
+        const paymentMethodsResponse = await fetch("/api/paymentMethods", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        }).then(response => response.json());
+
         const configuration = {
-            // Pass paymentMethodsResponse to configuration
             paymentMethodsResponse: paymentMethodsResponse,
             clientKey,
             locale: "en_US",
+            countryCode: 'NL',
             environment: "test",
             showPayButton: true,
-            paymentMethodsConfiguration: {
-                ideal: {
-                    showImage: true,
-                },
-                card: {
-                    hasHolderName: true,
-                    holderNameRequired: true,
-                    name: "Credit or debit card",
-                    amount: {
-                        value: 10000,
-                        currency: "EUR",
-                    },
-                },
-                paypal: {
-                    amount: {
-                        value: 10000,
-                        currency: "USD",
-                    },
-                    environment: "test", // Change this to "live" when you're ready to accept live PayPal payments
-                    countryCode: "US", // Only needed for test. This will be automatically retrieved when you are in production
-                    onCancel: (data, component) => {
-                        component.setStatus('ready');
-                    },
+            translations: {
+                'en-US': {
+                    'creditCard.securityCode.label': 'CVV/CVC'
                 }
             },
-            onSubmit: (state, component) => {
-                if (state.isValid) {
-                    handleSubmission(state, component, "/api/initiatePayment");
+            onSubmit: async (state, component, actions) => {
+                console.info("onSubmit", state, component, actions);
+                try {
+                    if (state.isValid) {
+                        const { action, order, resultCode } = await fetch("/api/payments", {
+                            method: "POST",
+                            body: state.data ? JSON.stringify(state.data) : "",
+                            headers: {
+                                "Content-Type": "application/json",
+                            }
+                        }).then(response => response.json());
+
+                        if (!resultCode) {
+                            console.warn("reject");
+                            actions.reject();
+                        }
+
+                        actions.resolve({
+                            resultCode,
+                            action,
+                            order
+                        });
+                    }
+                } catch (error) {
+                    console.error(error);
+                    actions.reject();
                 }
             },
-            onAdditionalDetails: (state, component) => {
-                if (state.isValid) {
-                    handleSubmission(state, component, "/api/submitAdditionalDetails");
-                }
+            onPaymentCompleted: (result, component) => {
+                console.info("onPaymentCompleted", result, component);
+                handleOnPaymentCompleted(result, component);
             },
-        };
-        const checkout = await new AdyenCheckout(configuration);
-        checkout.create(type).mount(document.getElementById("payment"));
-    } catch (error) {
-        console.error(error);
-        alert("Error occurred. Look at console for details");
-    }
-}
-
-// Event handlers called when the shopper selects the pay button,
-// or when additional information is required to complete the payment
-async function handleSubmission(state, component, url) {
-    try {
-        const res = await sendPostRequest(url, state.data);
-        handleServerResponse(res, component);
-    } catch (error) {
-        console.error(error);
-        alert("Error occurred. Look at console for details");
-    }
-}
-
-// Sends POST request to url
-async function sendPostRequest(url, data) {
-    const res = await fetch(url, {
-        method: "POST",
-        body: data ? JSON.stringify(data) : "",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
-
-    return await res.json();
-}
-
-// Handles responses sent from your server to the client
-function handleServerResponse(res, component) {
-    if (res.action) {
-        console.info(res);
-        component.handleAction(res.action);
-    } else {
-        switch (res.resultCode) {
-            case "Authorised":
-                window.location.href = "/result/success";
-                break;
-            case "Pending":
-            case "Received":
-                window.location.href = "/result/pending";
-                break;
-            case "Refused":
-                window.location.href = "/result/failed";
-                break;
-            default:
+            onPaymentFailed: (result, component) => {
+                console.info("onPaymentFailed", result, component);
+                handleOnPaymentFailed(result, component);
+            },
+            onError: (error, component) => {
+                console.error("onError", error.name, error.message, error.stack, component);
                 window.location.href = "/result/error";
-                break;
-        }
+            },
+            // Used for the Native 3DS2 Authentication flow, see: https://docs.adyen.com/online-payments/3d-secure/native-3ds2/
+            onAdditionalDetails: async (state, component, actions) => {
+                console.info("onAdditionalDetails", state, component);
+                try {
+                    const { resultCode } = await fetch("/api/payments/details", {
+                        method: "POST",
+                        body: state.data ? JSON.stringify(state.data) : "",
+                        headers: {
+                            "Content-Type": "application/json",
+                        }
+                    }).then(response => response.json());
+
+                    if (!resultCode) {
+                        console.warn("reject");
+                        actions.reject();
+                    }
+
+                    actions.resolve({ resultCode });
+                } catch (error) {
+                    console.error(error);
+                    actions.reject();
+                }
+            }
+        };
+
+        const paymentMethodsConfiguration = {
+            card: {
+                showBrandIcon: true,
+                hasHolderName: true,
+                holderNameRequired: true,
+                name: "Credit or debit card",
+                amount: {
+                    value: 10000,
+                    currency: "EUR",
+                },
+                placeholders: {
+                    cardNumber: '1234 5678 9012 3456',
+                    expiryDate: 'MM/YY',
+                    securityCodeThreeDigits: '123',
+                    securityCodeFourDigits: '1234',
+                    holderName: 'J. Smith'
+                }
+            }
+        };
+
+        // Start the AdyenCheckout and mount the element onto the 'payment' div.
+        const adyenCheckout = await AdyenCheckout(configuration);
+        const dropin = new Dropin(adyenCheckout, {
+            paymentMethodsConfiguration: paymentMethodsConfiguration
+        }).mount('#dropin-container');
+    } catch (error) {
+        console.error(error);
+        alert("Error occurred. Look at console for details.");
     }
 }
 
-initCheckout();
+// Function to handle payment completion redirects
+function handleOnPaymentCompleted(response) {
+    switch (response.resultCode) {
+        case "Authorised":
+            window.location.href = "/result/success";
+            break;
+        case "Pending":
+        case "Received":
+            window.location.href = "/result/pending";
+            break;
+        default:
+            window.location.href = "/result/error";
+            break;
+    }
+}
+
+// Function to handle payment failure redirects
+function handleOnPaymentFailed(response) {
+    switch (response.resultCode) {
+        case "Cancelled":
+        case "Refused":
+            window.location.href = "/result/failed";
+            break;
+        default:
+            window.location.href = "/result/error";
+            break;
+    }
+}
+
+startCheckout();
